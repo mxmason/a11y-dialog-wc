75f09241cb19b939263b4ec867884151526cbc19
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-07-14 03:01:49 -0700

    chore: adjust dev scripts

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import { terser } from 'rollup-plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
+const plugins = [nodeResolve(), typescript({sourceMap: true, tsconfig: './tsconfig.rollup.json' })]
+
+export default [
+  {
+    plugins,
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/a11y-dialog.js',
+        format: 'esm',
+        plugins: [terser()],
+      },
+    ],
+  },
+]
