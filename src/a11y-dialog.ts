cf752558a28bf8c29bdaf198185744b30413a2c8
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-07-19 13:14:46 -0700

    fix: do component registry more robustly

--- src/a11y-dialog.ts
+++ src/a11y-dialog.ts
@@ -1,3 +1,12 @@
-import { A11yDialog } from './A11yDialog.js';
+import { A11yDialog as A11yDialogElement } from './A11yDialog.js';
 
-window.customElements.define('a11y-dialog', A11yDialog);
+declare global {
+  interface Window {
+    A11yDialogElement: typeof A11yDialogElement
+  }
+}
+
+if (!window.customElements.get('a11y-dialog')) {
+  window.A11yDialogElement = A11yDialogElement
+  window.customElements.define('a11y-dialog', A11yDialogElement)
+}
